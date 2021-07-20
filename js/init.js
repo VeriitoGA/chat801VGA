564b056ec9a9aabf2f00691e803734beed0fd774
VeriitoGA <87164514+VeriitoGA@users.noreply.github.com>
2021-07-20 16:28:36 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyC6Vtj5B6gu81ORxj8DmTGNQleMIM8lZGI",
+    authDomain: "chat801vga.firebaseapp.com",
+    projectId: "chat801vga",
+    storageBucket: "chat801vga.appspot.com",
+    messagingSenderId: "541441697088",
+    appId: "1:541441697088:web:545761ee5b448d615186a5",
+    measurementId: "G-ECWFCQY5CL"
+    });
